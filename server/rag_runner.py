2a1a959b821df3944d39bfdbb207aa9f71628430
ahmed-jawad-5 <ahip1429@gmail.com>
2025-12-14 15:19:06 +0500

    RAG implementation (meta data bug fixing)

--- server/rag_runner.py
+++ server/rag_runner.py
@@ -1,5 +1,6 @@
 import os
 import pickle
+import joblib
 import faiss
 import torch
 from sentence_transformers import SentenceTransformer
@@ -38,8 +39,7 @@ class RAGRunner:
         self.index = faiss.read_index(index_path)
         print("[RAG] Metadata path:", meta_path)
         print("[RAG] File size:", os.path.getsize(meta_path), "bytes")
-        with open(meta_path, "rb") as f:
-            self.metadata = pickle.load(f, encoding="latin1")
+        self.metadata = joblib.load(meta_path)
 
 
 
